56127356201199a377dc31425296c94aa5935bad
GeneTv <contact@genetv.net>
2021-09-22 13:25:59 +0200

    Yee

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp({
+  render: () => {
+    template: `<router-view/>`
+  }
+}).use(store).use(router).mount('#app')
